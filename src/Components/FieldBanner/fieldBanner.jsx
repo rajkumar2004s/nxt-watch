0fe271b09813511dedc8c2806cc038a0c65ae0b9
Rajkumar <bhootlarajkumar@gmail.com>
2026-06-24 12:54:27 +0530

    fix: resolve case-sensitive fieldbanner.css import mismatch

--- src/Components/FieldBanner/fieldBanner.jsx
+++ src/Components/FieldBanner/fieldBanner.jsx
@@ -1,6 +1,6 @@
 import { useContext } from "react";
 
-import "./fieldBanner.css";
+import "./fieldbanner.css";
 import { ThemeContext } from "../../HOCs/ThemeContext/themeContext";
 
 const FieldBanner = (props) => {
